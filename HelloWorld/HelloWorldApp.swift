62c449bec7c0c10d9955ff50994d06feff7a0818
marrrdls <marrrdls.com@gmail.com>
2025-01-21 10:10:01 +0100

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Margarita on 21.1.25..
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
